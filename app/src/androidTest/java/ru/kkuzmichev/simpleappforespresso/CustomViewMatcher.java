7584f723db2531a48a6ff7a850138f34df1c3ae7
TatyanaRas <tatyanalipch@yandex.ru>
2025-01-21 13:24:47 +0300

    Второй комит

--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/CustomViewMatcher.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/CustomViewMatcher.java
@@ -6,21 +6,18 @@ import android.view.View;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.espresso.matcher.BoundedMatcher;
 
-import org.hamcrest.Matcher;
 import org.hamcrest.Description;
-//import org.junit.runner.Description;
+
 
 public class CustomViewMatcher {
-    public static Matcher<View> recyclerViewSizeMatcher(final int matcherSize) {
+
+   public static BoundedMatcher<View, RecyclerView> recyclerViewSizeMatcher(final int matcherSize) {
        return new BoundedMatcher<View, RecyclerView>(RecyclerView.class) {
 
-           private String result;
+        private String result;
 
-
-
-          @Override
-
-            public void describeTo(Description description) { // Доп. описание ошибки
+@Override
+        public void describeTo(Description description) { // Доп. описание ошибки
                 description.appendText("Requested list size: " + matcherSize);
                 description.appendText("Actual list size: ");
                 description.appendText(result);
--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/IdlingTest.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/IdlingTest.java
@@ -3,18 +3,16 @@ package ru.kkuzmichev.simpleappforespresso;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 import static java.util.regex.Pattern.matches;
 
-import static ru.kkuzmichev.simpleappforespresso.CustomViewMatcher.recyclerViewSizeMatcher;
-import static ru.kkuzmichev.simpleappforespresso.CustomViewAssertions.isRecyclerView;
 
-import android.view.View;
 
+import androidx.appcompat.widget.AppCompatImageButton;
 import androidx.test.espresso.IdlingRegistry;
 import androidx.test.espresso.ViewAssertion;
 import androidx.test.espresso.ViewInteraction;
@@ -22,7 +20,6 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
-import org.hamcrest.Matcher;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -31,8 +28,8 @@ import org.junit.runner.RunWith;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class IdlingTest {
 
+public class IdlingTest {
 
     @Rule
     public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
@@ -52,67 +49,25 @@ public class IdlingTest {
     }
 
 
-    @Test
-    public void openGalleryTest() {
+  @Test
+    public void testOpenGallery() {
 
-        ViewInteraction appCompatImageButton = onView(withContentDescription("Open navigation drawer"));
-        appCompatImageButton.check(matches(isDisplayed()));
-        appCompatImageButton.perform(click());
+        ViewInteraction menu = onView(isAssignableFrom(AppCompatImageButton.class));
 
-       ViewInteraction gallery = onView(withId(R.id.nav_gallery));
-        gallery.check(matches(isDisplayed()));
-        gallery.perform(click());
+      menu.check(matches(isDisplayed()));
+        menu.perform(click());
 
-    }
-
-
-    @Test
-    public void numberElementsTest() {
-        ViewInteraction appCompatImageButton = onView(withContentDescription("Open navigation drawer"));
-        appCompatImageButton.check(matches(isDisplayed()));
-        appCompatImageButton.perform(click());
 
         ViewInteraction gallery = onView(withId(R.id.nav_gallery));
-        gallery.check(matches(isDisplayed()));
         gallery.perform(click());
 
-        ViewInteraction recyclerView = onView(withId(R.id.recycle_view));
-        recyclerView.check(matches(isDisplayed()));
-        recyclerView.check(matches(CustomViewMatcher.recyclerViewSizeMatcher(10)));
-        recyclerView.check(CustomViewAssertions.isRecyclerView());
-    }
+       ViewInteraction recyclerView = onView(CustomViewMatcher.recyclerViewSizeMatcher(10));
+        recyclerView.check((ViewAssertion) isDisplayed());
+        recyclerView.check((CustomViewAssertions.isRecyclerView()));
 
-    @Test
-    public void sevenNumberTest() {
-        ViewInteraction appCompatImageButton = onView(withContentDescription("Open navigation drawer"));
-        appCompatImageButton.check(matches(isDisplayed()));
-        appCompatImageButton.perform(click());
-
-        ViewInteraction gallery = onView(withId(R.id.nav_gallery));
-        gallery.check(matches(isDisplayed()));
-        gallery.perform(click());
-
-        ViewInteraction itemSeven = onView(allOf(withId(R.id.item_number), withText("7")));
-        itemSeven.check(matches(isDisplayed()));
+        ViewInteraction itemSeven = onView((allOf(withId(R.id.item_number), withText("7"))));
         itemSeven.check(matches(withText("7")));
     }
-
-
-
-    @Test
-    public void listTest() {
-        ViewInteraction appCompatImageButton = onView(withContentDescription("Open navigation drawer"));
-        appCompatImageButton.check(matches(isDisplayed()));
-        appCompatImageButton.perform(click());
-
-        ViewInteraction gallery = onView(withId(R.id.nav_gallery));
-        gallery.check(matches(isDisplayed()));
-        gallery.perform(click());
-
-        ViewInteraction recyclerView = onView(withId(R.id.recycle_view));
-        recyclerView.check(CustomViewAssertions.isRecyclerView());
-    }
-
-    }
+}
 
 
